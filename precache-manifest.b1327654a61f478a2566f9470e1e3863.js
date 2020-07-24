d652e70b6ff61323e288a53f2f59819e276986b3
Sergio-Ka <chuidoo@gmail.com>
2020-07-24 23:30:42 +0700

    Updates

---
+++ precache-manifest.b1327654a61f478a2566f9470e1e3863.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d0f8ef7d87e20ca540ad1d63d4e4e0",
+    "url": "/pixlpark-test/index.html"
+  },
+  {
+    "revision": "0d5c24059901a1daf25d",
+    "url": "/pixlpark-test/static/css/main.51c9aa54.chunk.css"
+  },
+  {
+    "revision": "3c17f858499c3eaca98b",
+    "url": "/pixlpark-test/static/js/2.f551607b.chunk.js"
+  },
+  {
+    "revision": "1263c357d445340b9558207334972351",
+    "url": "/pixlpark-test/static/js/2.f551607b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d5c24059901a1daf25d",
+    "url": "/pixlpark-test/static/js/main.495976ba.chunk.js"
+  },
+  {
+    "revision": "1ff0f9c9bf8e00e249d0",
+    "url": "/pixlpark-test/static/js/runtime-main.e50f2e77.js"
+  }
+]);
